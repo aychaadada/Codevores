972400dfe933a89c3375e34c36166859404330ba
aycha75020 <60877811+aycha75020@users.noreply.github.com>
2020-04-01 16:31:21 +0200

    Mon premiere pas en PHP

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="container footer">
+      <p>&copy;Copyright 2020 - Codevores.co - &reg; Tous droits réservés.</p>
+    </div>
+  </footer>
---
+++ header.php
@@ -0,0 +1,16 @@
+<header <div id="banniere">  
+       <h2>Codevores</h2><br>
+       <pre>                       <p>une developpeuse à un clic</p> </pre>
+       <h1>Page de la communauté</h1>
+       <ul>
+              <li><a href="pagedinscription.php" >Page d'inscription</a></li>
+              <li><a href="pagedeconnection.php" >Page de connection</a></li>
+              <li><a href="espacerecruteur.php">Espace Recruteur</a></li>
+              <li><a href="espaceduprofessionnel.php">Espace Professionnel</a></li>
+              <li><a href="pagedelacommunaute.php">Page de la Communauté</a></li>
+              <li><a href="pageduprofessionnel.php">Page du Professionnel</a></li>
+              <li><a href="lesactualites.php">Les actualités</a></li>
+              <li><a href="contact.php">Contact</a></li>
+              <li><a href="faq.php">FAQ</a></li>
+       </ul>        
+</header>       
